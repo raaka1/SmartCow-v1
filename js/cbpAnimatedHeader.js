ebc042cbd82cc85767f607151cab0d1e06cc7eb3
Kyle Wardell <wardellsk@gmail.com>
2016-04-17 10:20:29 -0400

    removed nav-bar shrink

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -26,12 +26,6 @@ var cbpAnimatedHeader = (function() {
 
 	function scrollPage() {
 		var sy = scrollY();
-		if ( sy >= changeHeaderOn ) {
-			classie.add( header, 'navbar-shrink' );
-		}
-		else {
-			classie.remove( header, 'navbar-shrink' );
-		}
 		didScroll = false;
 	}
 
